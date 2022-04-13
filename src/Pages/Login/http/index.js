28a4681794e8cab04db406c8f5a301bcdf8e589c
ruslanSSSplus <ruslanlyanov1999@gmail.com>
2022-04-13 20:11:28 +0300

    bundlesd

--- src/Pages/Login/http/index.js
+++ src/Pages/Login/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 
-export const API_URL = `http://localhost:5000/api`
+export const API_URL = `https://beerauthback.herokuapp.com/api`
 
 const $api = axios.create({
     withCredentials: true,
@@ -18,7 +18,7 @@ $api.interceptors.response.use((config) => {
     return config;
 },async (error) => {
     const originalRequest = error.config;
-    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+    if (error.response.status === 401 && error.config && !error.config._isRetry) {
         originalRequest._isRetry = true;
         try {
             const response = await axios.get(`${API_URL}/refresh`, {withCredentials: true})
